8eb31364143f7cf3ca1627e8c594bed476c0d50c
goldf3219 <goldf3219@gmail.com>
2021-10-02 00:52:08 +0800

    2-JWT實作

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
+
 const express=require('express')
 const app=express()
+const jwt= require('jsonwebtoken')
+
+app.use(express.json())
 
 const postsnews=[
     {
@@ -12,8 +17,31 @@ const postsnews=[
     },
 ]
 
-app.get('/postsnews',(req,res)=>{
-    res.json(postsnews)
+app.get('/postsnews', authenticateToken,(req,res)=>{
+    res.json(postsnews.filter(postsnew=>postsnew.username === req.user.name))
 })
+
+app.post('/login', (req,res)=>{
+    //使用者驗證，產生accessToken
+
+    const username=req.body.username
+    const user={ name: username }
+
+    const accessToken= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+})
+
+//驗證token
+function authenticateToken( req, res, next){
+    const authHeader= req.headers['authorization']
+    const token= authHeader && authHeader.split(' ')[1]
+    if (token==null) return res.sendStatus(401)
+    //驗證jwt
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user)=>{
+        if(err) return res.sendStatus(403) //token超時、不正確等情況
+        req.user=user
+        next()
+    })
+}
 
 app.listen(3000)
